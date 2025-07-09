73b35567faa0a3c47b7aa64de4859941dc6aff22
Toem Sopheara <sopheara.toem@gmail.com>
2025-07-09 15:11:05 +0700

    Bio Updated

---
+++ src/components/ContentBio.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">ABOUT ME!</h2>
+      <p class="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+    </div>
+    <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <article class="flex max-w-xl flex-col items-start justify-between">
+        <!-- <div class="flex items-center gap-x-4 text-xs">
+          <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+          <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
+        </div> -->
+        <div class="group relative">
+          <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+            <a href="#">
+              <span class="absolute inset-0"></span>
+              Study Experiences:
+            </a>
+          </h3>
+          <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+           2009-2011 High School at KPS
+          </p>
+          <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+           2011-2016 Bachelor of Computer Engeering at RUPP
+          </p>
+        </div>
+        <div class="relative mt-8 flex items-center gap-x-4">
+          <img src="../assets/Photos/TSPR.jpg" alt="" class="size-10 rounded-full bg-gray-50" />
+          <div class="text-sm/6">
+            <p class="font-semibold text-gray-900">
+              <a href="#">
+                <span class="absolute inset-0"></span>
+                Student
+              </a>
+            </p>
+            <p class="text-gray-600">1999-2011</p>
+          </div>
+        </div>
+        
+      </article>
+
+      <!-- More posts... -->
+             <article class="flex max-w-xl flex-col items-start justify-between">
+        <!-- <div class="flex items-center gap-x-4 text-xs">
+          <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+          <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
+        </div> -->
+        <div class="group relative">
+          <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+            <a href="#">
+              <span class="absolute inset-0"></span>
+              Working Experiences:
+            </a>
+          </h3>
+          
+          <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+           2017-2019 working as official at MLMUPC
+          </p>
+          <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+           2019-2021 working as deputy chief of office at MLMUPC
+          </p>
+          <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+           2021-Now working as chief of office at MLMUPC
+          </p>
+        </div>
+        <div class="relative mt-8 flex items-center gap-x-4">
+          <img src="../assets/Photos/photo_2024-03-19_07-54-48.jpg" alt="" class="size-10 rounded-full bg-gray-50" />
+          <div class="text-sm/6">
+            <p class="font-semibold text-gray-900">
+              <a href="#">
+                <span class="absolute inset-0"></span>
+                Job
+              </a>
+            </p>
+            <p class="text-gray-600">2017-Now</p>
+          </div>
+        </div>
+        
+      </article>
+    </div>
+  </div>
+</div>
+</template>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,12 +1,15 @@
 <template>
     <HeroSection />
+    <content-bio />
 </template>
 <script>
 import HeroSection from '@/components/HeroSection.vue';
+import ContentBio from '@/components/ContentBio.vue';
 
 export default {
     components: {
-        HeroSection
+        HeroSection,
+        ContentBio
     }
 }
 </script>
