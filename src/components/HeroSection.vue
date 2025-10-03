90c678e3848f173f2beefd56bb4194fc45b7ae87
Sim Kuntheng <simkuntheng@gmail.com>
2025-10-03 14:30:09 +0700

    Change name in Herosection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">SOPHEARA TOEM</h1>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">SIM KUNTHENG</h1>
             <!-- <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                 <span class="font-bold">Short infos</span>
             </p> -->
