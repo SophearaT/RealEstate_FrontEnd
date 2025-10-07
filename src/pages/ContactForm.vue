3ec67bdda76afe5c967d64a44d1481572760b335
Sim Kuntheng <simkuntheng@gmail.com>
2025-10-07 22:26:01 +0700

    change contact form

--- src/pages/ContactForm.vue
+++ src/pages/ContactForm.vue
@@ -1,11 +1,12 @@
 <template>
     <section class="mx-4">
-        <div class="max-w-md mx-auto my-6 p-6 border border-cyan-200 rounded-2xl bg-cyan-300 shadow-2xl shadow-cyan-500/100">
-            <h2 class="text-2xl font-semibold mb-6 text-center">Contact Form</h2>
-            <Form :validation-schema="validateSchema" @submit="handleSubmit" class="space-y-4">
+        <div
+            class="w-full max-w-80 mx-auto my-6 p-6 border border-cyan-200 rounded-2xl bg-cyan-300 shadow-2xl shadow-cyan-500/100">
+            <h2 class="text-2xl font-semibold mb-4 text-center">Contact Form</h2>
+            <Form :validation-schema="validateSchema" @submit="handleSubmit">
                 <!-- Name Field -->
                 <div>
-                    <label for="name" class="block mb-1">Name</label>
+                    <label for="name" class="block mb-1 font-semibold">Name</label>
                     <Field type="text" v-model="form.name" name="name"
                         class="w-full px-4 py-2 border border-black-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-cyan-500"
                         placeholder="Enter your name" />
@@ -13,7 +14,7 @@
                 </div>
                 <!-- Email Field -->
                 <div>
-                    <label for="email" class="block mb-1">Email</label>
+                    <label for="email" class="block mb-1 font-semibold">Email</label>
                     <Field type="email" v-model="form.email" name="email"
                         class="w-full px-4 py-2 border border-black-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-cyan-500"
                         placeholder="Enter your email" />
@@ -21,7 +22,7 @@
                 </div>
                 <!-- Message Field -->
                 <div>
-                    <label for="message" class="block mb-1">Message</label>
+                    <label for="message" class="block mb-1 font-semibold">Message</label>
                     <Field as="textarea" v-model="form.message" name="message" rows="3"
                         class="w-full px-4 py-2 border border-black-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-cyan-500"
                         placeholder="Enter your message" />
@@ -30,7 +31,7 @@
                 <!-- Submit Button -->
                 <button type="submit"
                     class="w-full py-2 bg-black text-white rounded-2xl hover:bg-white hover:text-black hover:cursor-pointer transition-colors duration-300">
-                    Submit
+                    Send Message
                 </button>
             </Form>
         </div>
