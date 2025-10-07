d5735a8668d532467b274eb12e4bf7207cb5a660
Sim Kuntheng <simkuntheng@gmail.com>
2025-10-07 22:02:05 +0700

    change herosection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,19 +1,16 @@
 <template>
     <section class="bg-white dark:bg-gray-900">
-        <div class="w-full px-4">
-            <div class="place-self-center pt-8">
-                <h1 class="font-bold md:text-2xl dark:text-white text-center mb-4">
-                    About Our System</h1>
-                <p class="indent-10 md:text-lg lg:text-xl dark:text-white">
-                    Established in 2025, our system is the leading real estate platform in Cambodia. From our offices in
-                    Phnom Penh, we assist property seekers to find, inquire and secure properties for sale and rent. The
-                    website lists many properties for sale and rent across Cambodia.
-                </p>
-            </div>
-            <div class="lg:mt-8 lg:flex place-self-center py-8">
-                <img src="../assets/Photos/teamIdg3.jpg" alt="photo of our team">
-            </div>
+        <div class="place-self-center pt-8 px-8">
+            <h1 class="font-bold md:text-2xl dark:text-white text-center mb-4">
+                About Our System</h1>
+            <p class="indent-10 md:text-lg lg:text-xl dark:text-white">
+                Established in 2025, our system is the leading real estate platform in Cambodia. From our offices in
+                Phnom Penh, we assist property seekers to find, inquire and secure properties for sale and rent. The
+                website lists many properties for sale and rent across Cambodia.
+            </p>
+        </div>
+        <div class="lg:mt-8 lg:flex place-self-center py-8 px-8">
+            <img src="../assets/Photos/teamIdg3.jpg" alt="photo of our team">
         </div>
     </section>
 </template>
-
